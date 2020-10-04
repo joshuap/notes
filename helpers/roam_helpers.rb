e93409e7da5ff6c1c82b0473e2ea8e02b90ddef4
Joshua Wood <josh@joshuawood.net>
2020-10-04 11:02:53 -0700

    Use divs at top level (enables reader mode in Safari)

--- helpers/roam_helpers.rb
+++ helpers/roam_helpers.rb
@@ -12,42 +12,61 @@ module RoamHelpers
     end
   end
 
-  def render_page_content(data, page = data)
-    children = Array(data["children"])
+  def render_page_content(block, page = block)
+    children = Array(block["children"])
+    return "" if children.empty?
+
+    children.map { |block|
+      if block["heading"]
+        render_block(block, page) + render_page_content(block, page)
+      else
+        render_block(block, page) {
+          render_list(block, page)
+        }
+      end
+    }.join.html_safe
+  end
+
+  def render_list(block, page)
+    children = Array(block["children"])
     return "" if children.empty?
 
     content_tag(:ul) {
       children.map { |block|
-        content_tag(:li, id: "block-#{block["uid"]}") {
-          render_block(block, page) + render_page_content(block, page)
+        content_tag(:li) {
+          render_block(block, page) + render_list(block, page)
         }
       }.join
     }
   end
 
   def render_block(block, page)
-    content_tag(:div, class: block_class(block)) {
-      render_markdown(
-        link_refs(
-          block["string"].to_s,
-          page
-        ).gsub(/\A(?<name>[\w\s]+)::/) { |_match|
-          %(<span class="page-metadata">#{$1}:</span>)
-        }
-      ).gsub(/\(\((?<uid>[^()]+)\)\)/) {
-        uid = Regexp.last_match[:uid]
-        if (ref = Hash(block["outbound_block_references"])[uid])
-          render_markdown(
-            content_tag(:span, class: "block-ref") {
-              link_to(ref["string"], "/#{string_to_slug(ref["page_title"])}#block-#{uid}", data: {prefetch: true})
-            }
-          )
-        else
-          "((#{uid}))"
-        end
-      }.gsub(/\^\^(?<text>[^\^]+)\^\^/) {
-        content_tag(:span, Regexp.last_match[:text], class: "roam-highlight")
-      }.delete("```") # RedCarpet leaves the trailing ``` from Roam when code blocks don't end in a new line.
+    content = render_markdown(
+      link_refs(
+        block["string"].to_s,
+        page
+      ).gsub(/\A(?<name>[\w\s]+)::/) { |_match|
+        %(<span class="page-metadata">#{$1}:</span>)
+      }
+    ).gsub(/\(\((?<uid>[^()]+)\)\)/) {
+      uid = Regexp.last_match[:uid]
+      if (ref = Hash(block["outbound_block_references"])[uid])
+        render_markdown(
+          content_tag(:span, class: "block-ref") {
+            link_to(ref["string"], "/#{string_to_slug(ref["page_title"])}#block-#{uid}", data: {prefetch: true})
+          }
+        )
+      else
+        "((#{uid}))"
+      end
+    }.gsub(/\^\^(?<text>[^\^]+)\^\^/) {
+      content_tag(:span, Regexp.last_match[:text], class: "roam-highlight")
+    }.delete("```") # RedCarpet leaves the trailing ``` from Roam when code blocks don't end in a new line.
+
+    content << yield if block_given?
+
+    content_tag(:div, id: "block-#{block["uid"]}", class: block_class(block)) {
+      content
     }
   end
 
